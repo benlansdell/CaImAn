c2725fd552e794c30d0503851737c13389ac2908
benlansdell <ben.lansdell@stjude.org>
2022-06-16 16:46:44 -0500

    Change how it creates mmap filenames, works better with inputs files with h5 extension

--- caiman/motion_correction.py
+++ caiman/motion_correction.py
@@ -2825,11 +2825,17 @@ def motion_correct_batch_rigid(fname, max_shifts, dview=None, splits=56, num_spl
             save_movie = save_movie_rigid
             logging.debug('saving!')
 
+        #if isinstance(fname, tuple):
+        #    base_name=os.path.split(fname[0])[-1][:-4] + '_rig_'
+        #else:
+        #    base_name=os.path.split(fname)[-1][:-4] + '_rig_'
 
         if isinstance(fname, tuple):
-            base_name=os.path.split(fname[0])[-1][:-4] + '_rig_'
+            base_name = os.path.split(fname[0])[-1].split('.')[:-1]
+            base_name = '.'.join(base_name) + '_rig_'
         else:
-            base_name=os.path.split(fname)[-1][:-4] + '_rig_'
+            base_name = os.path.split(fname)[-1].split('.')[:-1]
+            base_name = '.'.join(base_name) + '_rig_'
 
         fname_tot_rig, res_rig = motion_correction_piecewise(fname, splits, strides=None, overlaps=None,
                                                              add_to_movie=add_to_movie, template=old_templ, max_shifts=max_shifts, max_deviation_rigid=0,
